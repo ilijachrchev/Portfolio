4ce46747c8e95bccb2304f6f33adb77772fa9a3a
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 22:50:58 +0200

    feat(github): map repository overview sections

---
+++ app/(site)/components/theme/github/githubRepository.js
@@ -0,0 +1,23 @@
+export const GITHUB_SECTIONS = [
+  {
+    id: 'overview',
+    label: 'Overview',
+    sectionId: 'home',
+    path: 'portfolio/',
+    kind: 'repository',
+  },
+  {
+    id: 'readme',
+    label: 'README.md',
+    sectionId: 'about',
+    path: 'README.md',
+    kind: 'readme',
+  },
+  {
+    id: 'community',
+    label: 'community',
+    sectionId: 'service',
+    path: 'community/',
+    kind: 'directory',
+  },
+]
