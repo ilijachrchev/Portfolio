27423e2552994742b53586072f3ab8b147bb6d05
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:35:45 +0200

    feat(win98): define initial utility windows

---
+++ app/(site)/components/theme/win98/windows98Windows.js
@@ -0,0 +1,23 @@
+export const WINDOWS98_WINDOWS = {
+  network: {
+    id: 'network',
+    title: 'Network Neighborhood',
+    icon: '/themes/win98/icons/network.svg',
+    position: { x: 180, y: 86 },
+    size: { width: 430, height: 280 },
+  },
+  recycle: {
+    id: 'recycle',
+    title: 'Recycle Bin',
+    icon: '/themes/win98/icons/recycle.svg',
+    position: { x: 250, y: 120 },
+    size: { width: 360, height: 220 },
+  },
+  help: {
+    id: 'help',
+    title: 'Portfolio Help',
+    icon: '/themes/win98/icons/info.svg',
+    position: { x: 210, y: 100 },
+    size: { width: 440, height: 300 },
+  },
+}
