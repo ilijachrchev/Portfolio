b2b50b0c2d1bab54e7b33b8d5c703b6f5efe834b
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:30:58 +0200

    feat(win98): connect desktop workspace provider

--- app/(site)/components/theme/win98/Windows98Experience.jsx
+++ app/(site)/components/theme/win98/Windows98Experience.jsx
@@ -1,10 +1,15 @@
 'use client'
 
 import styles from './Windows98Experience.module.css'
+import Windows98Desktop from './Windows98Desktop'
+import { Windows98WorkspaceProvider } from './useWindows98Workspace'
 
 export default function Windows98Experience() {
   return (
-    <div className={styles.desktop} data-win98-desktop aria-label="Windows 98 portfolio desktop">
-    </div>
+    <Windows98WorkspaceProvider>
+      <div className={styles.desktop} data-win98-desktop aria-label="Windows 98 portfolio desktop">
+        <Windows98Desktop />
+      </div>
+    </Windows98WorkspaceProvider>
   )
 }
