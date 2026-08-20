afd7946b7979afd60ecfb7de238ef11d30b9bcb1
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:40:14 +0200

    feat(win98): render managed utility windows

---
+++ app/(site)/components/theme/win98/Windows98Utilities.jsx
@@ -0,0 +1,31 @@
+import Windows98Network from './Windows98Network'
+import Windows98RecycleBin from './Windows98RecycleBin'
+import Windows98Window from './Windows98Window'
+import { useWindows98Workspace } from './useWindows98Workspace'
+
+const CONTENT = {
+  network: Windows98Network,
+  recycle: Windows98RecycleBin,
+}
+
+export default function Windows98Utilities() {
+  const { activeWindowId, closeWindow, focusWindow, minimizeWindow, toggleMaximizeWindow, windows } = useWindows98Workspace()
+
+  return Object.values(windows).map((window) => {
+    const Content = CONTENT[window.id]
+    if (!Content) return null
+    return (
+      <Windows98Window
+        key={window.id}
+        window={window}
+        active={activeWindowId === window.id}
+        onClose={() => closeWindow(window.id)}
+        onFocus={() => focusWindow(window.id)}
+        onMaximize={() => toggleMaximizeWindow(window.id)}
+        onMinimize={() => minimizeWindow(window.id)}
+      >
+        <Content />
+      </Windows98Window>
+    )
+  })
+}
