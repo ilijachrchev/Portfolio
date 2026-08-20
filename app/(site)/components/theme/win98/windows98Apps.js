61587018da2a529218eb16e374f526b0ab7c3301
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:28:57 +0200

    feat(win98): map portfolio section applications

--- app/(site)/components/theme/win98/windows98Apps.js
+++ app/(site)/components/theme/win98/windows98Apps.js
@@ -19,4 +19,22 @@ export const WINDOWS98_APPS = [
     sectionId: 'service',
     icon: `${ICON_ROOT}/folder.svg`,
   },
+  {
+    id: 'projects',
+    label: 'Projects',
+    sectionId: 'work',
+    icon: `${ICON_ROOT}/projects.svg`,
+  },
+  {
+    id: 'guestbook',
+    label: 'Guestbook',
+    sectionId: 'endorsements-home',
+    icon: `${ICON_ROOT}/folder.svg`,
+  },
+  {
+    id: 'contact',
+    label: 'Contact',
+    sectionId: 'contact',
+    icon: `${ICON_ROOT}/contact.svg`,
+  },
 ]
