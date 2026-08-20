59ddfa912942eb222aac4718cf1b70e083da6efe
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:40:30 +0200

    feat(win98): launch desktop utility windows

--- app/(site)/components/theme/win98/Windows98Experience.jsx
+++ app/(site)/components/theme/win98/Windows98Experience.jsx
@@ -3,6 +3,7 @@
 import styles from './Windows98Experience.module.css'
 import Windows98Desktop from './Windows98Desktop'
 import Windows98Taskbar from './Windows98Taskbar'
+import Windows98Utilities from './Windows98Utilities'
 import { Windows98WorkspaceProvider } from './useWindows98Workspace'
 
 export default function Windows98Experience() {
@@ -10,6 +11,7 @@ export default function Windows98Experience() {
     <Windows98WorkspaceProvider>
       <div className={styles.desktop} data-win98-desktop aria-label="Windows 98 portfolio desktop">
         <Windows98Desktop />
+        <Windows98Utilities />
         <Windows98Taskbar />
       </div>
     </Windows98WorkspaceProvider>
