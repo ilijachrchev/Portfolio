91d090ee4ab26f97ce0ac2d69ce914996ba41a82
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:28:44 +0200

    feat(win98): map overview desktop applications

---
+++ app/(site)/components/theme/win98/windows98Apps.js
@@ -0,0 +1,22 @@
+const ICON_ROOT = '/themes/win98/icons'
+
+export const WINDOWS98_APPS = [
+  {
+    id: 'computer',
+    label: 'My Computer',
+    sectionId: 'home',
+    icon: `${ICON_ROOT}/computer.svg`,
+  },
+  {
+    id: 'documents',
+    label: 'My Documents',
+    sectionId: 'about',
+    icon: `${ICON_ROOT}/folder.svg`,
+  },
+  {
+    id: 'community',
+    label: 'Community',
+    sectionId: 'service',
+    icon: `${ICON_ROOT}/folder.svg`,
+  },
+]
