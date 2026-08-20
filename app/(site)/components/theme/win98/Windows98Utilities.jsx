d897813e19a452b4618b6417cc414db566d60e08
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:48:19 +0200

    feat(win98): execute portfolio Run commands

--- app/(site)/components/theme/win98/Windows98Utilities.jsx
+++ app/(site)/components/theme/win98/Windows98Utilities.jsx
@@ -1,9 +1,11 @@
 import Windows98Help from './Windows98Help'
 import Windows98Network from './Windows98Network'
 import Windows98RecycleBin from './Windows98RecycleBin'
+import Windows98Run from './Windows98Run'
 import Windows98Winver from './Windows98Winver'
 import Windows98Window from './Windows98Window'
 import { useWindows98Workspace } from './useWindows98Workspace'
+import { getWindows98Command } from './windows98Commands'
 
 const CONTENT = {
   network: Windows98Network,
@@ -11,15 +13,29 @@ const CONTENT = {
 }
 
 export default function Windows98Utilities() {
-  const { activeWindowId, closeWindow, focusWindow, minimizeWindow, moveWindow, navigateToApp, openWindow, toggleMaximizeWindow, windows } = useWindows98Workspace()
+  const { activeWindowId, closeWindow, focusWindow, minimizeWindow, moveWindow, navigateToApp, openWindow, showError, toggleMaximizeWindow, windows } = useWindows98Workspace()
+
+  const executeCommand = (input) => {
+    const { command, action } = getWindows98Command(input)
+    closeWindow('run')
+    if (!action) {
+      showError(`Windows cannot find '${command}'. Try help, projects, contact, theme, or winver.`)
+      return
+    }
+    if (action.type === 'app') navigateToApp(action.value)
+    if (action.type === 'window') openWindow(action.value)
+    if (action.type === 'external') window.open(action.value, '_blank', 'noopener,noreferrer')
+  }
 
   return Object.values(windows).map((window) => {
     const Content = CONTENT[window.id]
-    if (!Content && !['help', 'winver'].includes(window.id)) return null
+    if (!Content && !['help', 'run', 'winver'].includes(window.id)) return null
     const content = window.id === 'help'
       ? <Windows98Help navigateToApp={navigateToApp} onOpenWinver={() => openWindow('winver')} />
       : window.id === 'winver'
         ? <Windows98Winver onClose={() => closeWindow('winver')} />
+        : window.id === 'run'
+          ? <Windows98Run onCancel={() => closeWindow('run')} onExecute={executeCommand} />
         : <Content />
     return (
       <Windows98Window
