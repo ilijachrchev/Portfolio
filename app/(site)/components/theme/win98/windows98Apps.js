a84518ca6a6cc4996931c59cfe8e6db2fdd9b281
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:29:11 +0200

    feat(win98): register desktop utility shortcuts

--- app/(site)/components/theme/win98/windows98Apps.js
+++ app/(site)/components/theme/win98/windows98Apps.js
@@ -38,3 +38,18 @@ export const WINDOWS98_APPS = [
     icon: `${ICON_ROOT}/contact.svg`,
   },
 ]
+
+export const WINDOWS98_UTILITIES = [
+  {
+    id: 'network',
+    label: 'Network Neighborhood',
+    icon: `${ICON_ROOT}/network.svg`,
+  },
+  {
+    id: 'recycle',
+    label: 'Recycle Bin',
+    icon: `${ICON_ROOT}/recycle.svg`,
+  },
+]
+
+export const WINDOWS98_SHORTCUTS = [...WINDOWS98_APPS, ...WINDOWS98_UTILITIES]
