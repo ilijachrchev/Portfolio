2e5545a1de11e9e211d0924b377210c3b8327241
ilijachrchev <ilijachrchev@gmail.com>
2026-08-20 21:30:42 +0200

    feat(win98): render selectable desktop shortcuts

---
+++ app/(site)/components/theme/win98/Windows98Desktop.jsx
@@ -0,0 +1,25 @@
+import Windows98DesktopIcon from './Windows98DesktopIcon'
+import { WINDOWS98_SHORTCUTS } from './windows98Apps'
+import { useWindows98Workspace } from './useWindows98Workspace'
+import styles from './Windows98Experience.module.css'
+
+export default function Windows98Desktop() {
+  const { selectedShortcut, setActiveAppId, setSelectedShortcut } = useWindows98Workspace()
+
+  return (
+    <div className={styles.iconGrid} aria-label="Desktop shortcuts">
+      {WINDOWS98_SHORTCUTS.map((shortcut) => (
+        <Windows98DesktopIcon
+          key={shortcut.id}
+          shortcut={shortcut}
+          selected={selectedShortcut === shortcut.id}
+          onSelect={() => setSelectedShortcut(shortcut.id)}
+          onLaunch={() => {
+            setSelectedShortcut(shortcut.id)
+            setActiveAppId(shortcut.id)
+          }}
+        />
+      ))}
+    </div>
+  )
+}
